091b334c34dcbda163f310c25ef20cafee0e2e3c
AshwiniKumar1103 <ashwini.kumar1fdbeb@cognizant.com>
2024-08-30 15:32:02 +0530

    multiple Threads

---
+++ TryCatch/BufferReaderAndScanner.java
@@ -0,0 +1,37 @@
+package TryCatch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class BufferReaderAndScanner {
+    public static void main(String[] args) throws IOException {
+        System.out.println("enter a number");
+        int num = System.in.read();
+        System.out.println(num - 48);
+        BufferedReader bf = null;
+        try {
+            System.out.println("enter second number");
+            clearInputStream();
+            InputStreamReader ins = new InputStreamReader(System.in);
+            bf = new BufferedReader(ins);
+            int num2 = Integer.parseInt(bf.readLine());
+            System.out.println(num2);
+            System.out.println("Enter third number");
+            clearInputStream();
+            Scanner sc = new Scanner(System.in);
+            int num3 = sc.nextInt();
+            System.out.println(num3);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            bf.close();
+        }
+    }
+
+    private static void clearInputStream() throws IOException {
+        while (System.in.available() > 0) {
+            System.in.read();
+        }
+    }
+}
--- TryCatch/TryCatchMethod.java
+++ TryCatch/TryCatchMethod.java
@@ -1,25 +1,29 @@
 package TryCatch;
 
+class MyException extends Exception {
+    public MyException(String ErrMsg) {
+        super(ErrMsg);
+    }
+}
+
 public class TryCatchMethod {
     public static void main(String[] args) {
-        int i = 10;
-        int j = 0;
-        int [] arr = new int [5];
+        int i = 20;
+        int j = 10;
+        int[] arr = new int[5];
         try {
-             j = 4/i;
-             if(j==0){
-                throw new ArithmeticException("j is having 0 value");
-             }
-             System.out.println(arr[6]);
+            j = j / i;
+            if (j == 0) {
+                throw new MyException("j is having 0 value");
+            }
+            System.out.println(arr[6]);
         } catch (ArithmeticException e) {
-            System.out.println("error is " + e.getMessage());
-        }
-        catch(ArrayIndexOutOfBoundsException e){
             System.out.println(e.getMessage());
-        }
-        catch(Exception e){
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
             System.out.println(e);
         }
-        System.out.println("Value of j is "+j);
+        System.out.println("Value of j is " + j);
     }
 }
